a57e89955c227112859ec75a8afa24e5deea0c09
ULRBf <143408302+ULRBf@users.noreply.github.com>
2024-05-27 16:26:46 +0900

    Update site.config.js
    change avatar image

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "ULRBf",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/smile_avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "",
     bio: "",
     email: "",
