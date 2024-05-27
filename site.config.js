28070bd1ceef3e1a2a0c2105a987aabb5fd9f49c
ULRBf <143408302+ULRBf@users.noreply.github.com>
2024-05-27 13:52:48 +0900

    Update site.config.js
    do tutorial

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "ULRBf",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "",
+    linkedin: "",
+    github: "",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `ULRBf-log`,
+      href: "https://github.com/ULRBf/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "ULRBf-log",
+    description: "welcome to ULRBf-log!",
   },
 
   // CONFIG configration (required)
@@ -53,9 +53,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "ULRBf/ULRBf-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
